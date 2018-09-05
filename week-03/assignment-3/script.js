b90b35d01d932b43db47506a01dd4626c1d18d83
siqister <siqi.zhu@gmail.com>
2018-09-05 22:09:19 +0900

    Complete assignment 3

--- week-03/assignment-3/script.js
+++ week-03/assignment-3/script.js
@@ -10,15 +10,29 @@ console.log('Assignment 3');
  */
 {
 	//2.1 
-	/* YOUR CODE HERE*/
+	console.groupCollapsed('Question 2.1');
+	for(let i = 0; i < 10; i ++){
+		console.log(10 - i);
+	}	
+	console.groupEnd();
 
 	//2.2
-	/* YOUR CODE HERE*/
+	console.groupCollapsed('Question 2.2');
+	for(let i = 0; i <= 500; i++){
+		if(i%100 === 0){
+			console.log(i);
+		}
+	}
+	console.groupEnd();
 
 	//2.3
 	const arr = [89, 23, 88, 54, 90, 0, 10];
 	//Log out the content of this array using a for loop
-	/* YOUR CODE HERE*/
+	console.groupCollapsed('Question 2.3');
+	for(let i = 0; i < arr.length; i++){
+		console.log(arr[i]);
+	}
+	console.groupEnd();
 }
 
 /*
@@ -31,16 +45,32 @@ console.log('Assignment 3');
 
 {
 	//4.1
-	const instructors = undefined; /* YOUR CODE HERE */
+	const instructors = [
+		{name:'Ashley', department:'computer science', tenure:10},
+		{name:'Ben', department:'design', tenure:2},
+		{name:'Carol', department:'design', tenure:3},
+	]; 
 
 	//4.2 
 	/* COMPLETE THE FUNCTION */
-	function computeAvgTenure(){
-		return;
+	function computeAvgTenure(instructors){
+		let total = 0;
+		for(let i = 0; i < instructors.length; i++){
+			total += instructors[i].tenure;
+		}
+		return total/instructors.length;
 	}
 
+	console.log(`Average tenure of the three instructor is ${computeAvgTenure(instructors)}`);
+
 	//4.3
-	/* YOUR CODE HERE */
+	instructors.push({
+		name: 'Dan',
+		department: 'humanities',
+		tenure: 5
+	});
+
+	console.log(`Average tenure after adding Dan is ${computeAvgTenure(instructors)}`);
 
 }
 
