cc0d65cdb0f251d4609019a72771bd5154976123
siqister <siqi.zhu@gmail.com>
2018-11-16 10:37:16 -0500

    Week 11 in class

--- week-x1/x1-1/script.js
+++ week-x1/x1-1/script.js
@@ -38,42 +38,6 @@ function renderCylindricalProjection(geo, dom){
 		.scale(200)
 		//.clipAngle(45)
 
-	console.group('Cylindrical projection properties');
-	console.log(`Scale: ${projection.scale()}`)
-	console.log(`Center: ${projection.center()}`)
-	console.log(`Translate: ${projection.translate()}`);
-	console.log(`Clip angle: ${projection.clipAngle()}`);
-	console.groupEnd();
-
-	//Create a geoPath generator
-	const pathGenerator = d3.geoPath(projection);
-
-	//Create a graticules generator
-	const graticules = d3.geoGraticule()
-
-	//Render geo path
-	plot.append('path')
-		.datum(geo)
-		.attr('d', pathGenerator);
-
-	//Render a single point
-	plot.append('circle')
-		.datum(lngLatBoston)
-		.attr('cx', function(d){ return projection(lngLatBoston)[0] })
-		.attr('cy', function(d){ return projection(lngLatBoston)[1] })
-		.attr('r', 6)
-		.style('stroke','black')
-		.style('stroke-width', '2px')
-		.style('fill', 'yellow');
-
-	//Render graticules
-	plot.append('path')
-		.attr('class', 'graticules')
-		.datum(graticules)
-		.attr('d', pathGenerator)
-		.style('stroke','#333')
-		.style('stroke-opacity', .2)
-		.style('stroke-width','1px')
 
 }
 
@@ -133,57 +97,6 @@ function renderAzimuthalMap(geo, dom){
 }
 
 function renderConicalProjection(geo, dom){
-	console.log('Render world map in conical projection');
-
-	//Append DOM
-	const w = dom.clientWidth;
-	const h = dom.clientHeight;
-	const plot = d3.select(dom).append('svg')
-		.attr('width', w)
-		.attr('height', h);
-
-	//Create a projection function
-	const projection = d3.geoConicEqualArea()
-		.translate([w/2, h/2])
-		.precision(0)
-		//.rotate([-lngLatBoston[0],-lngLatBoston[1],0])
-
-	console.group('Conical projection properties');
-	console.log(`Scale: ${projection.scale()}`)
-	console.log(`Center: ${projection.center()}`)
-	console.log(`Translate: ${projection.translate()}`);
-	console.groupEnd();
-
-	//Create a geoPath generator
-	const pathGenerator = d3.geoPath(projection);
-
-	//Render geo path
-	plot.append('path')
-		.datum(geo)
-		.attr('d', pathGenerator);
-
-	//Render a single point
-	plot.append('circle')
-		.datum(lngLatBoston)
-		.attr('cx', function(d){ return projection(lngLatBoston)[0] })
-		.attr('cy', function(d){ return projection(lngLatBoston)[1] })
-		.attr('r', 6)
-		.style('stroke','black')
-		.style('stroke-width', '2px')
-		.style('fill', 'yellow');
-
-	//Create a graticules generator
-	const graticules = d3.geoGraticule()
-
-	//Render graticules
-	plot.append('path')
-		.attr('class', 'graticules')
-		.datum(graticules)
-		.attr('d', pathGenerator)
-		.style('stroke','#333')
-		.style('stroke-opacity', .2)
-		.style('stroke-width','1px')
-		.style('fill','none')
 
 }
 
@@ -196,76 +109,6 @@ function renderCollection(geo, dom){
 	const plot = d3.select(dom).append('svg')
 		.attr('width', w)
 		.attr('height', h);
-
-	//Create a projection function
-	const projection = d3.geoMercator()
-		.translate([w/2, h/2])
-		.precision(0)
-		.fitExtent([[0,0],[w,h]], geo);
-
-	console.group('Cylindrical projection properties');
-	console.log(`Scale: ${projection.scale()}`)
-	console.log(`Center: ${projection.center()}`)
-	console.log(`Translate: ${projection.translate()}`);
-	console.log(`Clip angle: ${projection.clipAngle()}`);
-	console.groupEnd();
-
-	//Create a geoPath generator
-	const pathGenerator = d3.geoPath(projection);
-
-	//Create a graticules generator
-	const graticules = d3.geoGraticule()
-
-	//Render graticules
-	plot.append('path')
-		.attr('class', 'graticules')
-		.datum(graticules)
-		.attr('d', pathGenerator)
-		.style('stroke','#333')
-		.style('stroke-opacity', .2)
-		.style('stroke-width','1px');
-
-	//Here we render geoPath a little differently
-	plot.selectAll('.geo-path')
-		.data(geo.features)
-		.enter()
-		.append('path')
-		.attr('class','geo-path')
-		.attr('d', pathGenerator)
-		.on('mouseenter', function(d){
-			d3.select(this).transition().style('fill','red')
-		})
-		.on('mouseleave', function(d){
-			d3.select(this).transition().style('fill','black')
-		})
-		.filter(function(d){return d.properties.ISO_A3 === "COG"})
-		.style('stroke','yellow')
-		.style('stroke-width','1px')
-
-	//Use path.centroid to locate points
-	plot.selectAll('.geo-center')
-		.data(geo.features)
-		.enter()
-		.append('circle')
-		.attr('class','geo-center')
-		.each(function(d){
-			const xy = pathGenerator.centroid(d);
-			d3.select(this)
-				.attr('cx', xy[0])
-				.attr('cy', xy[1])
-		})
-		.attr('r', 2)
-		.style('fill','yellow');
-
-	//Render a single point
-	plot.append('circle')
-		.datum(lngLatBoston)
-		.attr('cx', function(d){ return projection(lngLatBoston)[0] })
-		.attr('cy', function(d){ return projection(lngLatBoston)[1] })
-		.attr('r', 6)
-		.style('stroke','black')
-		.style('stroke-width', '2px')
-		.style('fill', 'yellow');
 
 }
 
